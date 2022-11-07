9db00383f81f2b68f506b7762b4441b4a4280d1f
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-11-07 15:38:21 +0000

    Create 300VirtualMachine.sh

---
+++ 300VirtualMachine.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon
+
+sudo apt install virt-manager -y
+
+sudo virsh net-start default
+sudo virsh net-autostart default
