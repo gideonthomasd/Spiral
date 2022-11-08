72dbcc08d6983e4d09a4765a226510c963c0f22a
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-11-08 18:18:25 +0000

    Update 110Install.sh

--- 110Install.sh
+++ 110Install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install lolcat xfce4-terminal neofetch i3 polybar sxhkd rofi dmenu pcmanfm picom lxpolkit moc lxterminal mpv i3status geany lxappearance feh parcellite yt-dlp calcurse font-manager fonts-font-awesome lxtask exa pulsemixer
+sudo apt audacious install lolcat xfce4-terminal neofetch i3 polybar sxhkd rofi dmenu pcmanfm picom lxpolkit moc lxterminal mpv i3status geany lxappearance feh parcellite yt-dlp calcurse font-manager fonts-font-awesome lxtask exa pulsemixer
