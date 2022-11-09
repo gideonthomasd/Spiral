77c64f750bd14376ecf3108ce90f56f0c0a93bd1
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-11-09 12:49:01 +0000

    Update 110Install.sh

--- 110Install.sh
+++ 110Install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo apt install audacious lolcat xfce4-terminal neofetch pcmanfm picom lxpolkit moc lxterminal mpv geany lxappearance feh parcellite yt-dlp calcurse font-manager fonts-font-awesome lxtask exa pulsemixer
+
+sudo apt install python3-tk python3-pil -y
